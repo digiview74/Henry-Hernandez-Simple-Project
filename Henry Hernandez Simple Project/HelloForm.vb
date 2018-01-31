70a442fdc4ebdbd58392f9c68b78d1d339fda8f4
HenryHernandez <henry_hernandez74@outlook.com>
2018-01-31 03:52:18 -0800

    Initial commit

---
+++ Henry Hernandez Simple Project/HelloForm.vb
@@ -0,0 +1,26 @@
+﻿Public Class HelloForm
+    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label1.Click
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Me.Close()
+    End Sub
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        If Label1.Location.X + Label1.Width < 0 Then
+            Label1.Location = New Point(Me.Width, Label1.Location.Y)
+        Else
+            Label1.Location = New Point(Label1.Location.X - 10, Label1.Location.Y)
+        End If
+        If Me.Label1.ForeColor = SystemColors.ControlText Then
+            Me.Label1.ForeColor = Color.YellowGreen
+        Else
+            Me.Label1.ForeColor = SystemColors.ControlText
+        End If
+    End Sub
+
+    Private Sub HelloForm_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+End Class
---
+++ Henry Hernandez Simple Project/MainForm.vb
@@ -0,0 +1,9 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        ' Create an instance of MessageForm. 
+        Dim frmHelloForm As New HelloForm
+
+        ' Display the form in modal style. 
+        frmHelloForm.Show()
+    End Sub
+End Class
